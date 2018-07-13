d565501d3baf01d6ffe154bed2726103ef8d50e7
whjkm <452422845@qq.com>
2018-07-13 19:59:55 +0800

    add taobao
    add taobao

---
+++ Taobao/config.py
@@ -0,0 +1,9 @@
+# import pymongo
+# client = pymongo.MongoClient(host='localhost', post=27017)
+MONGO_URL = 'localhost'
+MONGO_DB = 'taobao'
+MONGO_TABLE = 'product'
+
+KEYWORD = '美食'
+MAX_PAGE = 100
+SERVICE_ARGS = []
